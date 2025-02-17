132b286515f6932451c9a1c7439df45232c2a6dc
Abdulkudus777 <Abdulkudusakanji777@gmail.com>
2025-02-17 21:17:55 +0000

    chore: initial project setup with React, Router, and global styles

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Shop from "./components/Shop";
+import Product from "./components/Product";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+
+const NavBar = () => {
+  return (
+    <>
+      <div className="nav-bar">
+        <div>
+          <NavLink to="/" className="logo">
+            LOGO
+          </NavLink>
+        </div>
+        <div className="nav-links">
+          <NavLink to="/home" className="nav-link">
+            Home
+          </NavLink>
+          <NavLink to="/about" className="nav-link">
+            About
+          </NavLink>
+          <NavLink to="/contact" className="nav-link">
+            Contact
+          </NavLink>
+          <NavLink to="/shop" className="nav-link">
+            Shop
+          </NavLink>
+          <NavLink to="/product" className="nav-link">
+            Product
+          </NavLink>
+
+          <input type="text" className="search-bar" placeholder="search" />
+          <FaShoppingCart  className="cart-icon"/>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
