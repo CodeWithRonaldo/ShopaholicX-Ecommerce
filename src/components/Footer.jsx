43c530e279a2061c95a714b924e667b5ab4ed74b
Abdulkudus777 <Abdulkudusakanji777@gmail.com>
2025-02-26 18:19:35 +0000

    feat: add and style footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import './Footer.css'
+import { FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { CiFacebook } from "react-icons/ci";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='footer-logo'>
+        <h1>ShopaholicX</h1>
+        
+      </div>
+    <div>
+      <ul className='footer-links'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>About</a></li>
+        <li><a href='#'>Contact</a></li>
+
+
+      </ul>
+      <div className='icon-container'>
+
+      <div className='social-icons'>
+        <h2>Follow us on social media</h2>
+        <a href='https://www.instagram.com/'> <FaInstagram className='icons'/></a>
+        <a href='https://www.twitter.com/'> <FaXTwitter className='icons'/></a>
+        <a href='https://www.facebook.com/'> <CiFacebook className='icons'/></a>
+
+      </div>
+      </div>
+      <div className='copyright'>
+        <hr/>
+        <p>&copy; 2025 ShopaholicX. All rights reserved.</p>
+      </div>
+    </div>
+    </div>
   )
 }
 
